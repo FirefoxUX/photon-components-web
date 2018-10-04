eb964d9f1f9b3ede5b3e5609ac833db577474377
Les Orchard <me@lmorchard.com>
2018-10-04 18:03:26 +0000

    Add an "everything" story for good measure

--- components/Button/stories.js
+++ components/Button/stories.js
@@ -110,4 +110,79 @@ storiesOf("Button", module)
         style={{ backgroundImage: `url(${imageSync})` }}
       />
     </Sample>
-  ));
+  ))
+  .add("Everything", () => (
+    <Sample>
+      
+      <a href="#" className="button">
+        Anchor
+      </a>
+      <button>Button</button>
+      
+      <a href="#" className="button button--primary">
+        Anchor
+      </a>
+      <button className="button--primary">Button</button>
+
+      <a
+        href="#"
+        className="button button--ghost"
+        aria-label="refresh"
+        style={{ backgroundImage: `url(${imageSync})` }}
+      />
+      <button
+        className="button button--ghost"
+        aria-label="refresh"
+        style={{ backgroundImage: `url(${imageSync})` }}
+      />
+
+      <a href="#" className="button button--micro">
+        Anchor
+      </a>
+      <button className="button--micro">Button</button>
+
+      <a href="#" className="button button--primary button--micro">
+        Anchor
+      </a>
+      <button className="button--primary button--micro">Button</button>
+
+      <a href="#" className="button button--puffy">
+        Anchor
+      </a>
+      <button className="button--puffy">Button</button>
+
+      <a href="#" className="button button--primary button--puffy">
+        Anchor
+      </a>
+      <button className="button--primary button--puffy">Button</button>
+
+      <a href="#" className="button" aria-disabled>
+        Anchor
+      </a>
+      <button disabled>Button</button>
+
+      <a href="#" className="button button--primary" aria-disabled>
+        Anchor
+      </a>
+      <button className="button--primary" disabled>
+        Button
+      </button>
+
+      <a
+        href="#"
+        className="button button--ghost"
+        aria-label="refresh"
+        aria-disabled
+        style={{ backgroundImage: `url(${imageSync})` }}
+      />
+      <button
+        className="button button--ghost"
+        aria-label="refresh"
+        disabled
+        style={{ backgroundImage: `url(${imageSync})` }}
+      />
+      
+    </Sample>
+  ))
+  
+  ;
